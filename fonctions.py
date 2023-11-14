e72b5f3cb6d4dd72696315384bf70a713a7ba1f9
HugoWiltEFREI <hugo.wilt@efrei.net>
2023-11-14 10:01:36 +0100

    Test

---
+++ fonctions.py
@@ -0,0 +1,65 @@
+import os
+def extractNomsPres(fichier):
+    for i in "0123456789":
+        fichier = fichier.replace(i, "")  # On supprime les valeurs numériques
+    return (fichier[11:-4])  # On effectue un slicing pour récupérer uniquement le nom
+
+def associationTxtPrenomPres(nom):
+    trouve = False  # Au début, on n'a pas trouvé le prénom, donc trouve = False
+    ListeNomsPres = [["Chirac", "Jacques"], ["Giscard dEstaing", "Valéry"], ["Hollande", "François"],
+                     ["Macron", "Emmanuel"], ["Mitterand", "François"],
+                     ["Sarkozy", "Nicolas"]]  # On créé une liste qui associe le nom des présidents avec leurs prénoms
+    for indice in range(len(ListeNomsPres)):  # On parcours la liste
+        if ListeNomsPres[indice][
+            0] == nom:  # On compare si le premier élément (indice 0) de chaques sous-tableaux est égal au nom
+            print("Le prénom du Président", nom, "est",  ListeNomsPres[indice][1])  # Si oui, on affiche le prénom et le nom
+            trouve = True
+    if trouve == False:
+        print("Inconnu au bataillon")  # Si non, on affiche que l'on n'a pas trouvé
+
+def list_of_files(directory, extension):
+    files_names = []  # On créé une liste vide
+    for filename in os.listdir(directory):  # On parcours les documents dans le directory
+        if filename.endswith(extension):
+            files_names.append(
+                filename)  # Si l'extension du document correspond avec celle demandée, on rajoute à la liste
+    files_names2 = []  # On créé une nouvelle liste
+    for indice2 in range(len(files_names)):
+        if extractNomsPres(
+                files_names[indice2]) not in files_names2:  # Si les nom n'est pas présent dans la liste, on le rajoute
+            files_names2.append(extractNomsPres(files_names[indice2]))
+    return files_names2
+
+def notLettre(car):
+    if ((car >= ' ' and car <= '/')) or (car >= ':' and car <= '?') or (car >= '[' and car <= '`') or (
+            car >= '{' and car <= '}') :
+        return True
+    else:
+        return False
+
+def createTokens(nomDiscours):
+    with open("speeches/{}".format(nomDiscours), "r", encoding=("utf8")) as f1, open(
+            "cleaned/cleaned_{}".format(nomDiscours[11:]), "w", encoding=("utf8")) as f2:
+        inMot = False
+        for car in f1.read():
+            if notLettre(car):
+                if inMot == True:
+                    f2.write(' ')
+                    inMot = False
+            else:
+                if car >= 'A' and car <= 'Z':
+                    f2.write(chr(ord(car) + ord('a') - ord('A')))
+                    inMot = True
+                elif ord(car) >= 192 and ord(car) <= 220:
+                    f2.write(chr(ord(car) + ord('é') - ord('É')))
+                else:
+                    f2.write(car)
+                    inMot = True
+
+def createCleanedFolder():
+    nomsDiscours = []
+    for nom in os.listdir("speeches"):
+        if nom.endswith("txt"):
+            nomsDiscours.append(nom)
+    for nom in nomsDiscours:
+        createTokens(nom)
---
+++ main.py
@@ -0,0 +1,27 @@
+from fonctions import *
+
+
+if __name__ == '__main__':
+    fct = str(input("Quelle fonction voulez-vous utiliser? : "))
+
+    if fct == "extractNomsPres" or fct == "extraction":  # Fonction qui sert à extraire le Nom d'un Président grâce au nom du document
+        print("Quel nom de fichier voulez vous extraire")  # On demande de quel document on veux connaitre le Président
+        fichier = str(input())  # On stock le nom du document de la variable nom
+        print(extractNomsPres(fichier))  # On éxécute la fonction avec la variable nom et on imprime
+
+    elif fct == "associationTxtPrenomPres" or fct == "association":
+        print("Quel est le nom du Président : ")
+        nom = str(input())
+        print(associationTxtPrenomPres(nom))
+
+
+    elif fct == "list_of_files":
+        directory = str(input("directory : "))
+        extension = str(input("extension : "))
+        print(list_of_files(directory, extension))
+
+    elif fct == "createCleanedFolder" or fct == "nettoyage":
+        createCleanedFolder()
+
+    else:
+        print("Fonction inconnue, README.txt pour plus d'informations.")
