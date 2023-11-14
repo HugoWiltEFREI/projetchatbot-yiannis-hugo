343c6aaad24f9404f2b241bc79afca8978800a7a
YiannisLeblanc <149399878+YiannisLeblanc@users.noreply.github.com>
2023-11-14 11:36:34 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -32,42 +32,35 @@ def list_of_files(directory, extension):
             files_names2.append(extractNomsPres(files_names[indice2]))
     return files_names2
 
-def notLettre(car):
-    if ((car >= ' ' and car <= '/')) or (car >= ':' and car <= '?') or (car >= '[' and car <= '`') or (
-            car >= '{' and car <= '}') :
+def identifySpeChr(car):
+    if ((car>=' ' and car<='/')) or (car>=':' and car<='?') or (car>='[' and car<='`') or (car>='{' and car<='}') or car=='\n':
         return True
     else:
         return False
-
-def createTokens(nomDiscours):
-    with open("speeches/{}".format(nomDiscours), "r", encoding=("utf8")) as f1, open(
-            "cleaned/cleaned_{}".format(nomDiscours[11:]), "w", encoding=("utf8")) as f2:
-        inMot = False
+    
+def createTokenFiles(nomDiscours):
+    with open("speeches/{}".format(nomDiscours),"r",encoding=("utf8")) as f1, open("cleaned/cleaned_{}".format(nomDiscours[11:]),"w",encoding=("utf8")) as f2:
+        inMot=False
         for car in f1.read():
-            if notLettre(car):
-                if inMot == True:
-                    f2.write(' ')
-                    inMot = False
+            if identifySpeChr(car):
+                if inMot==True:
+                    f2.write('\n')
+                    inMot=False
             else:
-                inMot = minuscule(car, f2, inMot)
-
-
-def minuscule(car, f2, inMot):
-    if car >= 'A' and car <= 'Z':
-        f2.write(chr(ord(car) + ord('a') - ord('A')))
-        inMot = True
-    elif ord(car) >= 192 and ord(car) <= 220:
-        f2.write(chr(ord(car) + ord('é') - ord('É')))
-    else:
-        f2.write(car)
-        inMot = True
-    return inMot
-
-
+                if car>='A' and car<='Z':
+                    f2.write(chr(ord(car)+ord('a')-ord('A')))
+                    inMot=True
+                elif ord(car)>=192 and ord(car)<=220:
+                    f2.write(chr(ord(car)+ord('é')-ord('É')))
+                else:
+                    f2.write(car)
+                    inMot=True
 def createCleanedFolder():
-    nomsDiscours = []
+    nomsDiscours=[]
     for nom in os.listdir("speeches"):
         if nom.endswith("txt"):
             nomsDiscours.append(nom)
     for nom in nomsDiscours:
-        createTokens(nom)
+        createTokenFiles(nom)
+
+createCleanedFolder()
