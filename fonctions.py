1a477aadc43c49ef0c322685953694b899655443
YiannisLeblanc <149399878+YiannisLeblanc@users.noreply.github.com>
2023-11-14 11:39:47 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -31,36 +31,3 @@ def list_of_files(directory, extension):
                 files_names[indice2]) not in files_names2:  # Si les nom n'est pas présent dans la liste, on le rajoute
             files_names2.append(extractNomsPres(files_names[indice2]))
     return files_names2
-
-def identifySpeChr(car):
-    if ((car>=' ' and car<='/')) or (car>=':' and car<='?') or (car>='[' and car<='`') or (car>='{' and car<='}') or car=='\n':
-        return True
-    else:
-        return False
-    
-def createTokenFiles(nomDiscours):
-    with open("speeches/{}".format(nomDiscours),"r",encoding=("utf8")) as f1, open("cleaned/cleaned_{}".format(nomDiscours[11:]),"w",encoding=("utf8")) as f2:
-        inMot=False
-        for car in f1.read():
-            if identifySpeChr(car):
-                if inMot==True:
-                    f2.write('\n')
-                    inMot=False
-            else:
-                if car>='A' and car<='Z':
-                    f2.write(chr(ord(car)+ord('a')-ord('A')))
-                    inMot=True
-                elif ord(car)>=192 and ord(car)<=220:
-                    f2.write(chr(ord(car)+ord('é')-ord('É')))
-                else:
-                    f2.write(car)
-                    inMot=True
-def createCleanedFolder():
-    nomsDiscours=[]
-    for nom in os.listdir("speeches"):
-        if nom.endswith("txt"):
-            nomsDiscours.append(nom)
-    for nom in nomsDiscours:
-        createTokenFiles(nom)
-
-createCleanedFolder()
