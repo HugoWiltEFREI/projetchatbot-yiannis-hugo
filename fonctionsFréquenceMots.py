a3a684e6f7aa11bf15efeec3bb2d2adcf91fe693
YiannisLeblanc <149399878+YiannisLeblanc@users.noreply.github.com>
2023-11-21 15:43:24 +0100

    Update fonctionsFréquenceMots.py

--- fonctionsFréquenceMots.py
+++ fonctionsFréquenceMots.py
@@ -1,34 +1,34 @@
 import os
 #La fonction remplie dans vectors le dictionnaire d'occurrence de chaque mot du fichier file
-def createVector(file,vectors):
+def createVector(file,vectors,directory):
     nom=file[8:-4]
     vectors[nom]={}
-    with open("cleaned/{}".format(file),'r',encoding="utf8") as file:
+    with open("{}/{}".format(directory,file),'r',encoding="utf8") as file:
         for mot in file.readlines():
             if mot[:-1] in vectors[nom].keys():
                 vectors[nom][mot[:-1]]+=1
             else:
                 vectors[nom][mot[:-1]]=1
-#La fonction renvoie un tuple des noms de chaque fichier du dossier cleaned        
-def getCleanedFilesNames():
+#La fonction renvoie un tuple des noms de chaque fichier du dossier directory       
+def getCleanedFilesNames(directory):
     list=[]
-    for file in os.listdir("cleaned"):
+    for file in os.listdir(directory):
         if file.endswith(".txt"):
             list.append(file)
     return tuple(list)
 #La fonction renvoie les vecteur sous forme de dicos
 #Dico sous forme : [nom président + numéro] -> Mot -> nbOccurrence
-def getVectorsDico():
+def getVectorsDico(directory):
     vectors={}
-    filesList=getCleanedFilesNames()
+    filesList=getCleanedFilesNames(directory)
     for file in filesList:
-        createVector(file,vectors)
+        createVector(file,vectors,directory)
     return vectors
 #Renvoie dictionnaire de l'ensemble des mots et leurs occurrences tout fichier confondu
 # Mot -> Occurence
-def getWordsOccurrenceDico():
+def getWordsOccurrenceDico(directory):
     occur={}
-    vectors=getVectorsDico()
+    vectors=getVectorsDico(directory)
     for discours in vectors.keys():
         for mot in vectors[discours].keys():
             if mot in occur:
@@ -36,3 +36,12 @@ def getWordsOccurrenceDico():
             else:
                 occur[mot]=vectors[discours][mot]
     return occur
+
+while True:
+    mot=input("Saisir mot : ")
+    if mot in getWordsOccurrenceDico("cleaned").keys():
+        print(getWordsOccurrenceDico("cleaned")[mot])
+    else:
+        print(0)
+
+
