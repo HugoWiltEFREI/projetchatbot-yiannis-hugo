25edaf0f6cd3b23c474d290c3679eba1a6ce2a63
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-11-21 14:57:51 +0100

    Delete fonctionsTF-IDF.py

--- fonctionsTF-IDF.py
+++
@@ -1,9 +0,0 @@
-from math import  log10
-dicoSomme = {"test":1,"pomme":2}
-dicoRes = {}
-
-def inverseDocumentFrequency(directory):
-    for cle in dicoSomme.keys():
-        print(dicoSomme[cle])
-
-
