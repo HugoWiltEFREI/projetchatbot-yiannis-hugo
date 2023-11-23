a7ac1450914152e3c27fb9c0c9b36180d03425b1
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-11-23 18:24:56 +0100

    Update fonctionsTFIDF.py

--- fonctionsTFIDF.py
+++ fonctionsTFIDF.py
@@ -1,7 +1,7 @@
 import os
 from math import log10
 
-def nombreOccurrence(chaineDeC): #On définie la fct nombreOccurrence avec comme argument chaine de caractère
+def termFrequency(chaineDeC): #On définie la fct nombreOccurrence avec comme argument chaine de caractère
     nombreOcc = dict()  #On créer un dico vide où l'on va stocker le nombre d'occurrence des mots dans la chaine
     listeDeMots = chaineDeC.split() #On délimite les mots en regardant où sont les espaces et on les stocks dans un liste
     for mots in listeDeMots:
