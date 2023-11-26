1690702d788f9887de57376b7553b2513e68f064
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-11-26 15:05:11 +0100

    Update fonctionsDeBase.py

--- fonctionsDeBase.py
+++ fonctionsDeBase.py
@@ -1,10 +1,11 @@
 import os
+
+
 def extractNomsPres(fichier):
-    if type(fichier) != str:
-        raise("Illegal type for vairable fichier. fichier must be a str.")
     for i in "0123456789":
         fichier = fichier.replace(i, "")  # On supprime les valeurs numériques
-    return (fichier[11:-4])  # On effectue un slicing pour récupérer uniquement le nom
+    return fichier[11:-4]  # On effectue un slicing pour récupérer uniquement le nom
+
 
 def associationTxtPrenomPres(nom):
     trouve = False  # Au début, on n'a pas trouvé le prénom, donc trouve = False
@@ -12,12 +13,13 @@ def associationTxtPrenomPres(nom):
                      ["Macron", "Emmanuel"], ["Mitterand", "François"],
                      ["Sarkozy", "Nicolas"]]  # On créé une liste qui associe le nom des présidents avec leurs prénoms
     for indice in range(len(ListeNomsPres)):  # On parcours la liste
-        if ListeNomsPres[indice][
-            0] == nom:  # On compare si le premier élément (indice 0) de chaques sous-tableaux est égal au nom
-            print("Le prénom du Président", nom, "est",  ListeNomsPres[indice][1])  # Si oui, on affiche le prénom et le nom
+        if ListeNomsPres[indice][0] == nom:  # On compare si le premier élément (indice 0) de chaques sous-tableaux est égal au nom
+            print("Le prénom du Président", nom, "est",
+                  ListeNomsPres[indice][1])  # Si oui, on affiche le prénom et le nom
             trouve = True
-    if trouve == False:
-        return("Inconnu au bataillon")  # Si non, on affiche que l'on n'a pas trouvé
+    if not trouve:
+        return "Inconnu au bataillon"  # Si non, on affiche que l'on n'a pas trouvé
+
 
 def list_of_files(directory, extension):
     files_names = []  # On créé une liste vide
@@ -27,8 +29,7 @@ def list_of_files(directory, extension):
                 filename)  # Si l'extension du document correspond avec celle demandée, on rajoute à la liste
     files_names2 = []  # On créé une nouvelle liste
     for indice2 in range(len(files_names)):
-        if extractNomsPres(files_names[indice2]) not in files_names2:  # Si les nom n'est pas présent dans la liste, on le rajoute
+        if extractNomsPres(
+                files_names[indice2]) not in files_names2:  # Si les nom n'est pas présent dans la liste, on le rajoute
             files_names2.append(extractNomsPres(files_names[indice2]))
     return files_names2
-
-
