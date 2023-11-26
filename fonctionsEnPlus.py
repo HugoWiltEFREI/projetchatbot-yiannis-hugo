f009aac135e61ad8e41b4b80f8d879a3f2bc9b81
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-11-26 15:05:41 +0100

    Update fonctionsEnPlus.py

--- fonctionsEnPlus.py
+++ fonctionsEnPlus.py
@@ -1,6 +1,7 @@
 from fonctionsTFIDF import termFrequency, getCleanedFilesNames
 import matplotlib.pyplot as plt
 
+
 def plot(directory, val):
     dico2 = {}
     filesList = getCleanedFilesNames(directory)
@@ -8,11 +9,10 @@ def plot(directory, val):
         with open("{}/{}".format(directory, file), 'r', encoding="utf8") as discours:
             dico = termFrequency(discours.read())
             for k, v in dico.items():
-                if (v > val):
+                if v > val:
                     dico2[k] = v
             lists = sorted(dico2.items())
 
-            x, y = zip(*lists)  # unpack a list of pairs into two tuples
+            x, y = zip(*lists)
             plt.plot(x, y)
             plt.show()
-
