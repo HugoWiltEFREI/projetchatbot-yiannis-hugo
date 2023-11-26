f7fcecf5c7dcb362604b86fedfab11a14162a5ab
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-11-26 15:07:14 +0100

    Update fonctionsTFIDF.py

--- fonctionsTFIDF.py
+++ fonctionsTFIDF.py
@@ -1,54 +1,55 @@
 import os
 from math import log10
 
-def termFrequency(chaineDeC): #On définie la fct nombreOccurrence avec comme argument chaine de caractère
-    nombreOcc = dict()  #On créer un dico vide où l'on va stocker le nombre d'occurrence des mots dans la chaine
-    listeDeMots = chaineDeC.split() #On délimite les mots en regardant où sont les espaces et on les stocks dans un liste
+
+def termFrequency(chaineDeC):  # On définie la fct nombreOccurrence avec comme argument chaine de caractère
+    nombreOcc = dict()  # On créer un dico vide où l'on va stocker le nombre d'occurrence des mots dans la chaine
+    listeDeMots = chaineDeC.split()  # On délimite les mots en regardant où sont les espaces et on les stocks dans un liste
     for mots in listeDeMots:
         if mots in nombreOcc:
-            nombreOcc[mots] += 1 #Si le mot est déjà dans le dico, on fait +1
+            nombreOcc[mots] += 1  # Si le mot est déjà dans le dico, on fait +1
         else:
-            nombreOcc[mots] = 1 #Sinon on rajoute le mot dans le dico avec la valeur 1
+            nombreOcc[mots] = 1  # Sinon on rajoute le mot dans le dico avec la valeur 1
     return nombreOcc
 
-#La fonction renvoie un tuple des noms de chaque fichier du dossier directory
+
+# La fonction renvoie un tuple des noms de chaque fichier du dossier directory
 def getCleanedFilesNames(directory):
-    list=[]
+    listeCleaned = []
     for file in os.listdir(directory):
         if file.endswith(".txt"):
-            list.append(file)
-    return tuple(list)
+            listeCleaned.append(file)
+    return tuple(listeCleaned)
+
 
 def dicoIDF(directory):
-    dicoMots={}
-    nbFichier=0
+    dicoMots = {}
+    nbFichier = 0
     filesList = getCleanedFilesNames(directory)
     for discours in filesList:
-        nbFichier+=1
-        with open("{}/{}".format(directory,discours),'r',encoding="utf8") as file:
-            setMots=set()
+        nbFichier += 1
+        with open("{}/{}".format(directory, discours), 'r', encoding="utf8") as file:
+            setMots = set()
             for mot in file.read().split():
-                if not(mot in setMots):
+                if not (mot in setMots):
                     setMots.add(mot)
                     if mot in dicoMots.keys():
-                        dicoMots[mot]+=1
+                        dicoMots[mot] += 1
                     else:
-                        dicoMots[mot]=1
+                        dicoMots[mot] = 1
     for mot in dicoMots.keys():
-        dicoMots[mot]=log10(nbFichier/dicoMots[mot])
+        dicoMots[mot] = log10(nbFichier / dicoMots[mot])
     return dicoMots
 
 
 def matriceTFIDF(directory):
-    TFIDF={}
-    TF={}
-    filesList=getCleanedFilesNames(directory)
-    IDF=dicoIDF(directory)
+    TFIDF = {}
+    filesList = getCleanedFilesNames(directory)
+    IDF = dicoIDF(directory)
     for file in filesList:
-        with open("{}/{}".format(directory,file),'r',encoding="utf8") as discours:
-            TF=termFrequency(discours.read())
-        TFIDF[file[:-4]]={}
+        with open("{}/{}".format(directory, file), 'r', encoding="utf8") as discours:
+            TF = termFrequency(discours.read())
+        TFIDF[file[:-4]] = {}
         for mot in TF:
-            TFIDF[file[:-4]][mot]=TF[mot]*IDF[mot]
+            TFIDF[file[:-4]][mot] = TF[mot] * IDF[mot]
     return TFIDF
-
