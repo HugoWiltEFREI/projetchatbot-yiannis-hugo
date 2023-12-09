3c5e5d7939a46a0d104e10e8ec8e1854c6fcc880
Yiannis Leblanc <yiannis.leblanc@efrei.net>
2023-12-09 21:57:22 +0100

    Tokenize strings function

---
+++ inputProcessing.py
@@ -0,0 +1,27 @@
+# Il est impossible de modifier la fct "createTokenFile()" pour cette étape, le fctmnt est trop différent
+from fonctionsCleanedFolder import identifySpeChr
+def tokenList(text):
+    tokens=[]
+    index=-1
+    inMot=False
+    for car in text:
+        if identifySpeChr(car):
+            if inMot:
+                inMot = False
+        else:
+            if not inMot:
+                tokens.append("")
+                index+=1
+                inMot=True
+            lettreMinuscule(car, tokens, index)
+    return tokens
+
+def lettreMinuscule(car, tokensList, index):
+    if 'A' <= car <= 'Z':
+        tokensList[index]+=(chr(ord(car) + ord('a') - ord('A')))
+    elif 192 <= ord(car) <= 220:
+        tokensList[index]+=(chr(ord(car) + ord('é') - ord('É')))
+    elif car == 'Œ' or car == 'œ':
+        tokensList[index]+=("oe")
+    else:
+        tokensList[index]+=(car)
