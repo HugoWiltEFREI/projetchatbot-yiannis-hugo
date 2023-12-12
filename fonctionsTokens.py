cff1fdcc0df3a90de12dadc2af435fc4a50d6b99
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-12-12 10:10:23 +0100

    Update fonctionsTokens.py

--- fonctionsTokens.py
+++ fonctionsTokens.py
@@ -51,7 +51,46 @@ def tailleMatrice(directory):
                 if keys not in dico2:
                     dico2[keys] = 1
     return sum(dico2[item] for item in dico2)
-print(tailleMatrice("cleaned"))
 
-def matriceDicoToMatriceList(matrice):
-    print("hello")
+
+def creationMatrice(directory):
+    L = []
+    nbLig = len(getCleanedFilesNames(directory))+1
+    nbCol = tailleMatrice(directory)
+    for i in range(nbCol):
+        L.append([0.0]*nbLig)
+    return L
+
+def matriceDicoToMatriceList(matrice,directory):
+    liste = creationMatrice(directory)
+    matriceA = matriceTFIDF(directory)
+    i = 0
+    j = 0
+    k = 0
+    for val in matrice.values():
+        i+=1
+        for keys, values in val.items():
+            if liste[j][0] != 0 and isInList(keys,liste)==False:
+                j+=1
+            elif liste[j][0] == 0 and isInList(keys,liste)==False:
+                liste[j][0] = keys
+                liste[j][i] = round(values,3)
+                if j<(len(liste)-1):
+                    j+=1
+            elif isInList(keys,liste)==True:
+                while liste[k][0] != keys and k<(len(liste)-1):
+                    k = k+1
+                liste[k][i] = round(values,3)
+            k = 0
+    return liste
+
+def isInList(element,liste):
+    for i in range(len(liste)):
+        if liste[i][0] == element:
+            return True
+    else:
+        return False
+
+print(matriceDicoToMatriceList(matriceTFIDF("cleaned"),"cleaned"))
+
+
