7b44a593c7827832b836737cf73837f425feaceb
Yiannis Leblanc <yiannis.leblanc@efrei.net>
2023-12-16 18:18:28 +0100

    update semi importante

--- fonctionsSimilarity.py
+++ fonctionsSimilarity.py
@@ -11,14 +11,17 @@ def normeVector(vector):
     for mot in vector.keys():
         norme+=vector[mot]**2
     norme=sqrt(norme)
-    return norme
+    if norme!=0:
+        return norme
+    else:
+        return 1
 
 def similarityScore(vectorA,vectorB):
     score=produitScalaire(vectorA,vectorB)/(normeVector(vectorA)*normeVector(vectorB))
     return score
 
 def bestDocument(vectorQuestion,matriceCorpus):
-    meilleurs={"nom":None,"score":0}
+    meilleurs={"nom":"Aucun discours ne correspond à cette question.","score":0}
     scoreDoc=0
     for document in matriceCorpus.keys():
         scoreDoc=similarityScore(vectorQuestion,matriceCorpus[document])
@@ -26,3 +29,8 @@ def bestDocument(vectorQuestion,matriceCorpus):
             meilleurs["nom"]=document
             meilleurs["score"]=scoreDoc
     return meilleurs["nom"]
+
+
+import TFIDFQuestion,fonctionsTFIDF
+while True :
+    print(bestDocument(TFIDFQuestion.vectorTFIDFQuestion(input("Saisir la question : "),"cleaned"),fonctionsTFIDF.matriceTFIDF("cleaned")))
