5eb44c1df624bb93c58d4c2f5dc0dd62a3814fcf
Yiannis Leblanc <yiannis.leblanc@efrei.net>
2023-12-16 19:14:39 +0100

    finission de la réponse

--- fonctionsSimilarity.py
+++ fonctionsSimilarity.py
@@ -28,9 +28,24 @@ def bestDocument(vectorQuestion,matriceCorpus):
         if scoreDoc>meilleurs["score"]:
             meilleurs["nom"]=document
             meilleurs["score"]=scoreDoc
-    return meilleurs["nom"]
+    return "Nomination_{}.txt".format(meilleurs["nom"])
 
+def highestScoreWord(vectorQuestion):
+    word={"mot":None,"score":0}
+    score=0
+    for mot in vectorQuestion.keys():
+        score=vectorQuestion[mot]
+        if vectorQuestion[mot]>word["score"]:
+            word["mot"]=mot
+            word["score"]=score
+    return word["mot"]
 
-import TFIDFQuestion,fonctionsTFIDF
-while True :
-    print(bestDocument(TFIDFQuestion.vectorTFIDFQuestion(input("Saisir la question : "),"cleaned"),fonctionsTFIDF.matriceTFIDF("cleaned")))
+def rechercheFirstOccurenc(mot, document):
+    occurrence = 0
+    motMin = mot.lower()
+    with open("speeches/{}".format(document), "r", encoding="utf8") as f1:
+        liste = f1.read().lower().split("\n")
+        for i in range(len(liste)):
+            val = liste[i].find(motMin)
+            if val != -1:
+                return liste[i]
