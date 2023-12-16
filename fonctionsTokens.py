3626f4f1366e831a4483dd60e2d1e3de945c5e45
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2023-12-16 18:54:34 +0100

    Update fonctionsTokens.py

--- fonctionsTokens.py
+++ fonctionsTokens.py
@@ -1,6 +1,6 @@
 from fonctionsTFIDF import dicoIDF, matriceTFIDF
 from fonctionsCleanedFolder import identifySpeChr
-def tokenQuestion(chaineDeCToken:str):
+def tokenList(chaineDeCToken:str):
     for c in chaineDeCToken:
         if identifySpeChr(c):
             chaineDeCToken = chaineDeCToken.replace(c, " ")
@@ -10,36 +10,28 @@ def tokenQuestion(chaineDeCToken:str):
     return questionToken
 
 def rechercheCorpus(chaineDeCRecherche:str, directory):
-    valPresente = []
-    for ele in tokenQuestion(chaineDeCRecherche):
+    valPresente = set()
+    for ele in tokenList(chaineDeCRecherche):
         if (ele in dicoIDF(directory)) and (ele not in valPresente):
-            valPresente.append(ele)
+            valPresente.add(ele)
     return valPresente
 
+def scoreTF(mot,question):
+    score=0
+    listeTokens=tokenList(question)
+    for token in listeTokens:
+        if token==mot:
+            score+=1
+    return score
 
-def vecteurTF(question:str,directory):
-    dicoVecteurTF = {}
-    tokQue = tokenQuestion(question)
-    long = len(tokQue)
-    recherche = rechercheCorpus(question, directory)
-    for mot in tokQue:
-        if (mot in recherche) and (mot in dicoVecteurTF.keys()):
-            dicoVecteurTF[mot] += 1 / long
-        elif mot in recherche and mot not in dicoVecteurTF.keys():
-            dicoVecteurTF[mot] = 1 / long
-        else:
-            dicoVecteurTF[mot] = 0
-    return dicoVecteurTF
-
-def vecteurTFIDF(question:str,directory):
-    dicoVecteurTFIDF = {}
-    matrice = matriceTFIDF(directory)
-    for keys,values in vecteurTF(question,directory).items():
-        for vals in matrice.values():
-            for ke, va in vals.items():
-                if keys == ke:
-                    dicoVecteurTFIDF[keys] = values*va
-    return dicoVecteurTFIDF
-
+def vectorTFIDFQuestion(question,corpusDirectory):
+    matrice=matriceTFIDF(corpusDirectory)
+    IDF=dicoIDF(corpusDirectory)
+    vectorTFIDFQuestion={}
+    for discours in matrice.keys():
+        for mot in matrice[discours].keys():
+            if not mot in vectorTFIDFQuestion.keys():
+                vectorTFIDFQuestion[mot]=scoreTF(mot,question)*IDF[mot]
+    return vectorTFIDFQuestion
 
 
